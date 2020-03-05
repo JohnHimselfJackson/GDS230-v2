3b2a9ca01be0d14f8aa351f5c2f458f9dbc9cc92
MacVP <48820988+MacVP@users.noreply.github.com>
2020-03-05 11:50:12 +0800

    Macs Assets

---
+++ GDS230 Shoot'em Up/Assets/Macs Stuff/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float fireRate;
+    private float nextFire;
+
+    public Transform muzzle;
+    public GameObject bulletPrefab;
+    public bool shooting;
+
+    void Update()
+    {
+        //Fire rate
+        if (shooting && Time.time > nextFire)
+        {
+            nextFire = Time.time + (1 / fireRate);
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+    }
+
+    public void StartShooting()
+    {
+        shooting = true;
+        print("Shooting Started");
+    }
+
+    public void StopShooting()
+    {
+        print("Shooting Started");
+        shooting = false;
+    }
+}
---
+++ GDS230 Shoot'em Up/Assets/Macs Stuff/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    public float armour;
+
+    public void Damage(float damage)
+    {
+        //Takes damage from health
+        health = health - damage;
+
+        if (health <= 0)
+        {
+            //Invoke PlayerDead for animation duration
+            PlayerDead();
+        }
+    }
+
+    void PlayerDead()
+    {
+        //Revive animation
+        //Opt-in ads
+        //End game menu
+        //Pause Gameplay
+    }
+
+    public void Heal(float healAmount)
+    {
+        health += maxHealth * healAmount;
+        
+        if(health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+}
