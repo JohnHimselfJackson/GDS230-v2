9b9e4d4fe72c29671ebaa735449450e8c7e874c9
JohnTheManHimselfJackson <55519290+JohnTheManHimselfJackson@users.noreply.github.com>
2020-04-22 16:53:05 +0800

    prallax push and other stuff

---
+++ Assets/background/parallax.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    private float length, startPos;
+    public GameObject camGo;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = camGo.transform.position.x * (1 - parallaxEffect);
+        float distance = camGo.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if(temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
