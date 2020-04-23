2bb06eb82b4055b06ea472dc0027240b805ff67f
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2020-04-23 13:44:56 +0800

    MainMenuUpdate

---
+++ Assets/Brayden's Stuff/_Scripts/ObjectSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSetter : MonoBehaviour
+{
+    public GameObject[] myObjects;
+
+    public GameObject[] myotherObjects;
+
+    public void SetActive()
+    {
+        for (int i = 0; i < myObjects.Length; i++)
+        {
+            myObjects[i].SetActive(true);
+        }
+    }
+
+    public void SetInActive()
+    {
+        myotherObjects[0].SetActive(false);
+        myotherObjects[1].SetActive(false);
+        myotherObjects[2].SetActive(false);
+
+    }
+
+
+}
---
+++ Assets/Scripts/Player/SetImplant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetImplant : MonoBehaviour
+{
+    public ImplantAbility myImplant;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myImplant = GetComponent<ImplantAbility>();
+    }
+
+    public void SetDash()
+    {
+        myImplant.implant = ImplantAbility.Implant.BioticDash;
+    }
+
+    public void SetShield()
+    {
+        myImplant.implant = ImplantAbility.Implant.Shield;
+    }
+}
